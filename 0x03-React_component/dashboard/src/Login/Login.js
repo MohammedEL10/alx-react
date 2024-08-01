c6bf81f1a03e910277ade8e31717a636daebc8f6
MohammedEL10 <maohammedelhathout14@gmail.com>
2024-08-01 23:40:01 +0100

    0x03-React_component

--- 0x03-React_component/dashboard/src/Login/Login.js
+++ 0x03-React_component/dashboard/src/Login/Login.js
@@ -8,20 +8,23 @@ export default class App extends Component {
 				<div className="App">
 					<body className="App-body">
 						<p>Login to access the full dashboard</p>
-						<label htmlFor="email" onClick={() => {
-							// select corresponding input
-							document.getElementById('password').focus();
-						}}>Email</label>
-						<input type="email" id="email" />
-						<label htmlFor="password" onClick={() => {
-							// select corresponding input
-							document.getElementById('password').focus();
-						}}>Password</label>
-						<input type="password" id="password" />
-						<button>OK</button>
+						<div className="inputs">
+							<label htmlFor="email" onClick={() => {
+								// select corresponding input
+								document.getElementById('password').focus();
+							}}>Email</label>
+							<input type="email" id="email" />
+							<label htmlFor="password" onClick={() => {
+								// select corresponding input
+								document.getElementById('password').focus();
+							}}>Password</label>
+							<input type="password" id="password" />
+							<button>OK</button>
+						</div>
 					</body>
 				</div>
 			</React.Fragment>
 		)
 	}
 }
+
--- 0x03-React_component/dashboard/src/Notifications/Notifications.test.js
+++ 0x03-React_component/dashboard/src/Notifications/Notifications.test.js
@@ -1,5 +1,6 @@
 import { shallow } from 'enzyme';
 import Notifications from './Notifications';
+import NotificationItem from './NotificationItem';
 
 
 describe('<Notifications />', () => {
@@ -8,13 +9,40 @@ describe('<Notifications />', () => {
 		expect(wrapper.exists()).toBe(true);
 	})
 
-	it('tests that Notifications renders three list items', () => {
+	it('Checks first Item renders correct html', () => {
 		const wrapper = shallow(<Notifications />);
-		expect(wrapper.find('li').length).toBe(3);
+		expect(wrapper.text()).toContain('Your notifications');
 	})
 
-	it('tests that Notifications renders the text "Here is the list of notifications"', () => {
+	it('Tests that menuItem is rendered when displayDrawer is false', () => {	
+		const wrapper = shallow(<Notifications displayDrawer={false} />);
+		expect(wrapper.find('.menuItem').length).toBe(1);
+	})
+
+	it('Tests the div Notifications is not rendered when displayDrawer is false', () => {
+		const wrapper = shallow(<Notifications displayDrawer={false} />);
+		expect(wrapper.find('.Notifications').length).toBe(0);
+	})
+
+	it('Tests that menuItem is rendered when displayDrawer is true', () => {
+		const wrapper = shallow(<Notifications displayDrawer listNotifications={[]} />);
+		expect(wrapper.find('.menuItem').length).toBe(1);
+	})
+
+	it('Tests that the div Notifications is rendered when displayDrawer is true', () => {
+		const wrapper = shallow(<Notifications displayDrawer listNotifications={[]} />);
+		wrapper.update()
+		const item = wrapper.find('div.Notifications');
+		expect(item.length).toBe(1);
+	})
+
+	it('Tests when passing empty array', () => {
+		const wrapper = shallow(<Notifications notifications={[]} />);
+		expect(wrapper.find('.NotificationItem').length).toBe(0);
+	})
+
+	it('Tests when passing NO array', () => {
 		const wrapper = shallow(<Notifications />);
-		expect(wrapper.find('p').text()).toBe('Here is the list of notifications');
+		expect(wrapper.find('.NotificationItem').length).toBe(0);
 	})
 });
